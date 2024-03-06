11dfb14fb4ff302debf174cd1dd72012d80c0222
Ezequiel <ezequiel.moraes@vcodes.io>
2024-03-06 12:10:45 -0300

    resete css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './Css/tailwind.css'
+import './Css/reset.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
