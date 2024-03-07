a436d376d5b5ce08d6f67ba7c80ca278e53a5834
Ezequiel Magalhães <135054246+ezeqcode@users.noreply.github.com>
2024-03-06 21:05:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,12 @@ function App() {
         <div className="flex flex-col w-full justify-center items-center text-center">
           <span className="text-center justify-center items-center w-[90%] md:w-[90%] text-gray-600 md:text-lg font-normal  leading-6 md:leading-loose tracking-tight">
             Estamos realizando uma manutenção programada em nosso painel atual
-            para melhorar ainda mais a sua experiência. Enquanto isso, você pode
-            acessar todas as suas funcionalidades no painel antigo através do
-            endereço <span className="text-green-500"> cmswarez.tv</span>.
-            Agradecemos a sua compreensão e paciência. Em breve, você poderá
+            para melhorar ainda mais a sua experiência. Em breve, você poderá
             retornar ao painel atualizado com novidades e melhorias.
           </span>
         </div>
 
-        <a
-          href="https://cmswarez.tv/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex gap-2 no-underline justify-center items-center p-3 bg-green-500 rounded-lg hover:text-white hover:bg-green-600"
-        >
-          Acessar cmswarez.tv
-          <FaArrowRightLong />
-        </a>
+       
       </div>
     </div>
   );
