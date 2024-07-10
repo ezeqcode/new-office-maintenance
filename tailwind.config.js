805fd0e63b3c62557c360f9154f6123b45dcb0b7
Pedro Henrique Bandeira <pedro.bandeira@vcodes.io>
2024-07-10 15:22:20 -0300

    ph redirect tvs

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
         "custom-gradient-home":
           "radial-gradient(circle at center, rgba(7, 168, 93, 0.09) 1%, rgba(217, 217, 217, 0.00) 300%), linear-gradient(#111827, #111827)",
         "custom-gradient-home-light":
-          "radial-gradient(circle at center, rgba(7, 168, 93, 0.06) 1%, rgba(255, 255, 255, 0.00) 300%), linear-gradient(#F9FAFB, #F9FAFB)",
+          "radial-gradient(circle at center, rgba(88, 7, 168, 0.10) 1%, rgba(255, 255, 255, 0.00) 300%), linear-gradient(#F9FAFB, #F9FAFB)",
       },
     },
   },
